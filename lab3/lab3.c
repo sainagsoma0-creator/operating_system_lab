1923f645b07bb6eb96c19e3b402872a014a4ee7b
sainagsoma0-creator <sainagsoma0@gmail.com>
2026-02-10 15:00:50 +0530

    Create lab3.c

---
+++ lab3/lab3.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    int bt[10], pr[10], p[10];
+    int wt[10], tat[10];
+    int temp;
+    float avg_wt = 0, avg_tat = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        p[i] = i + 1;
+        printf("Enter Burst Time and Priority for P%d: ", p[i]);
+        scanf("%d %d", &bt[i], &pr[i]);
+    }
+
+    /* Sort processes based on Priority (Lower number = Higher priority) */
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(pr[i] > pr[j]) {
+                temp = pr[i];
+                pr[i] = pr[j];
+                pr[j] = temp;
+
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+
+    /* Waiting Time calculation */
+    wt[0] = 0;
+    for(i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];
+    }
+
+    /* Turnaround Time calculation */
+    for(i = 0; i < n; i++) {
+        tat[i] = wt[i] + bt[i];
+        avg_wt += wt[i];
+        avg_tat += tat[i];
+    }
+
+    avg_wt /= n;
+    avg_tat /= n;
+
+    printf("\nProcess\tBT\tPriority\tWT\tTAT\n");
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t\t%d\t%d\n",
+               p[i], bt[i], pr[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", avg_wt);
+    printf("\nAverage Turnaround Time = %.2f\n", avg_tat);
+
+    return 0;
+}
